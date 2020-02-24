bed89889f1edaa775d194fa14444e2129e2e455e
miaaaasong <60946220+miaaaasong@users.noreply.github.com>
2020-02-24 03:10:45 -0500

    Add files via upload

---
+++ Environmental data logger_program.py
@@ -0,0 +1,33 @@
+# Add your Python code here. E.g.
+from microbit import *
+import os
+# compass calibration
+if not compass.is_calibrated():
+    compass.calibrate()
+
+# main loop
+while True:
+    # prepare data
+    comp = compass.heading()
+    temp = temperature()
+    content = str(comp) + ',' + str(temp)
+    # optional addition(first part)
+    display.scroll(content, wait=False, loop=True)
+    file_list = os.listdir()
+    # if data.csv exist
+    if 'data.csv' in file_list:
+        # load current file
+        data_in = open('data.csv', 'r')
+        old = data_in.read()
+        data_in.close()
+        # update the data file
+        data_out = open('data.csv', 'w')
+        data_out.write(old + content + '\n')
+        data_out.close()
+    # if data.csv exist, build a file and save the data
+    else:
+        data_out = open('data.csv', 'w')
+        data_out.write(content + '\n')
+        data_out.close()
+    sleep(10000)
+    
